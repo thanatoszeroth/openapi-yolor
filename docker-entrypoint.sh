af6adbeea5c506a78910dfbc5cae33b5aebfd22f
hsiaocl <thanatoshsiao@gmail.com>
2022-09-29 16:40:31 +0800

    update 2022 0929 1640

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,16 @@
 echo "========== ========== =========="
 echo "Init ENV"
-if [ ${ServiceDomainName} == '' ]
+if [ ${ServiceIP} == '' ]
 then
-  ServiceDomainName = 0.0.0.0
+  ServiceIP = 0.0.0.0
 fi
-if [ ${ServiceDomainPort} == '' ]
+if [ ${ServicePort} == '' ]
 then
-  ServiceDomainPort = 19191
+  ServicePort = 19191
 fi
-echo "Service listening on http://${ServiceDomainName}:${ServiceDomainPort}"
+echo "Service listening on http://${ServiceIP}:${ServicePort}"
 echo "========== ========== =========="
 cd /app
 echo "Run API YOLOR Service"
-uvicorn main:app --host ${ServiceDomainName} --port ${ServiceDomainPort}
+uvicorn main:app --host ${ServiceIP} --port ${ServicePort}
 echo "========== ========== =========="
